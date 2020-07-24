84a0446ff5b01b0e4baa9f0944de964133d55aaa
Stas <bloodofabyss@yandex.ru>
2020-07-24 21:24:22 +0300

    Добавил роутеры для ошибок, карточек и пользователей

---
+++ routes/cards.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const { getCards, getCard } = require("../helpers/cards");
+
+router.get("/", getCards);
+
+router.get("/:id", getCard);
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const { getUsers, getUser } = require("../helpers/users");
+
+router.get("/", getUsers);
+
+router.get("/:id", getUser);
+
+module.exports = router;
