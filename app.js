1174625b9986240013026f45151de4bbc7cdf9fb
Stas <bloodofabyss@yandex.ru>
2020-07-24 21:32:14 +0300

    Собрал app.js

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
-const { PORT = 3000, BASE_PATH } = process.env;
+const { PORT = 3000 } = process.env;
 const path = require('path');
 
+const cards = require('./routes/cards');
+const users = require('./routes/users');
+const error = require('./routes/error');
+
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/cards', cards);
+app.use('/users', users);
+app.use("*", error);
 
 app.listen(PORT, () => {
   console.log(`Слушаю порт ${PORT}`);
 })
-
-app.use(express.static(path.join(__dirname, 'public')));
