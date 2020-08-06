c3638a050725623bf725f2a73632dd09258ad0e0
Stas <bloodofabyss@yandex.ru>
2020-08-06 18:23:15 +0300

    Обновил и добавил роуты для пользователей и карточек

--- routes/cards.js
+++ routes/cards.js
@@ -1,6 +1,17 @@
 const router = require('express').Router();
-const getCards = require('../helpers/cards');
+const {
+  getCards,
+  createCard,
+  deleteCard,
+  likeCard,
+  dislikeCard,
+} = require('../controllers/cards');
 
 router.get('/', getCards);
+router.post('/', createCard);
+router.delete('/', deleteCard);
+
+router.put('/:cardId/likes', likeCard);
+router.delete('/:cardId/likes', dislikeCard);
 
 module.exports = router;
