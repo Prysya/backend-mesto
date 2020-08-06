81770c9e0da8f3f9f2d59f471992513d75750452
Stas <bloodofabyss@yandex.ru>
2020-08-06 18:16:55 +0300

    Удалил папку data и public

--- helpers/checkFile.js
+++
@@ -1,8 +0,0 @@
-const fsPromises = require('fs').promises;
-
-const checkFile = (path) => fsPromises
-  .readFile(path, { encoding: 'utf8' })
-  .then((data) => data)
-  .catch((err) => err);
-
-module.exports = checkFile;
