7444eedf633c7300ff803f5a5500e4ffbda015da
Stas <bloodofabyss@yandex.ru>
2020-08-06 18:24:19 +0300

    Подключил body-parser и mongoose

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
-
-const path = require('path');
 
 const cards = require('./routes/cards');
 const users = require('./routes/users');
@@ -10,10 +10,24 @@ const errors = require('./routes/errors');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/cards', cards);
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '5f2c0a783f104207e78039cc',
+  };
+
+  next();
+});
 app.use('/users', users);
+app.use('/cards', cards);
 app.use('*', errors);
 
 app.listen(PORT, () => {
