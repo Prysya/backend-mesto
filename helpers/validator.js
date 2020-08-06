b58b67b15f5d9e6d368e0fa5518269ce47a60ff2
Stas <bloodofabyss@yandex.ru>
2020-08-06 18:19:43 +0300

    Добавил валидатор, для проверки ссылки

---
+++ helpers/validator.js
@@ -0,0 +1,7 @@
+module.exports.validator = (link) => {
+  const regExp = new RegExp(
+    '(https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|www\\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9]+\\.[^\\s]{2,}|www\\.[a-zA-Z0-9]+\\.[^\\s]{2,})',
+  );
+
+  return regExp.test(link);
+};
