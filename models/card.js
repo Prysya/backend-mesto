de89198432718860a878a955ed357c7320379ccf
Stas <bloodofabyss@yandex.ru>
2020-08-06 18:21:23 +0300

    Добавил схемы и модели для карточек и пользователей

---
+++ models/card.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const { validator } = require('../helpers/validator');
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    validate: {
+      validator: (link) => validator(link),
+      message: 'Неправильный формат ссылки',
+    },
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      default: [],
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('card', cardSchema);
