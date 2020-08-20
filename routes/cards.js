f19183e072bf9aff4bb31e9f6c1a98f065aa2e19
Stas <bloodofabyss@yandex.ru>
2020-08-20 21:23:31 +0300

    Обновил роуты карточек и пользователей

--- routes/cards.js
+++ routes/cards.js
@@ -9,7 +9,7 @@ const {
 
 router.get('/', getCards);
 router.post('/', createCard);
-router.delete('/', deleteCard);
+router.delete('/:cardId', deleteCard);
 
 router.put('/:cardId/likes', likeCard);
 router.delete('/:cardId/likes', dislikeCard);
--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,12 @@ const router = require('express').Router();
 const {
   getUsers,
   getUser,
-  createUser,
   updateUserData,
   updateUserAvatar,
 } = require('../controllers/users');
 
 router.get('/', getUsers);
 router.get('/:id', getUser);
-router.post('/', createUser);
 router.patch('/me', updateUserData);
 router.patch('/me/avatar', updateUserAvatar);
 
