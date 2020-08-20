a091fb540cf9dbfd9f868d6376ef98fff35be03d
Stas <bloodofabyss@yandex.ru>
2020-08-20 21:24:40 +0300

    Добавил coockieParser и мидлвар авторизации

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const { PORT = 3000 } = process.env;
 
 const cards = require('./routes/cards');
 const users = require('./routes/users');
 const errors = require('./routes/errors');
+const { login, createUser } = require('./controllers/users');
+const auth = require('./middlewares/auth');
 
 const app = express();
 
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -19,13 +23,11 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
 });
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '5f2c0a783f104207e78039cc',
-  };
+app.post('/signin', login);
+app.post('/signup', createUser);
 
-  next();
-});
+app.use(auth);
+
 app.use('/users', users);
 app.use('/cards', cards);
 app.use('*', errors);
