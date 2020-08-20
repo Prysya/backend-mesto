f9a990060fc8447483b174438554b10e6cc06200
Stas <bloodofabyss@yandex.ru>
2020-08-20 21:26:05 +0300

    Добавил поля email, passoword, написал static функцию проверки логин/пароля

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -23,6 +24,40 @@ const userSchema = new mongoose.Schema({
     },
     required: true,
   },
+  email: {
+    type: String,
+    validate: {
+      validator(email) {
+        return validator.isEmail(email);
+      },
+    },
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    select: false,
+  },
 });
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email })
+    .select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильная почта или пароль'));
+      }
+
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new Error('Неправильная почта или пароль'));
+        }
+
+        return user;
+      });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
