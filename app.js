4b29d4fb6fd14e54b9b7f3dc5bb93ca18b943836
Stas <bloodofabyss@yandex.ru>
2020-08-21 21:46:15 +0300

    Добавил и подключил helmet, rateLimit, dotenv

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+
+require('dotenv').config();
 
 const { PORT = 3000 } = process.env;
 
@@ -13,9 +17,13 @@ const auth = require('./middlewares/auth');
 
 const app = express();
 
+const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 100 });
+
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(limiter);
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
