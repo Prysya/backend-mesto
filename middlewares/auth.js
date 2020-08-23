4237f78f64d535e9f1a6b8774875770bfe5a9567
Stas <bloodofabyss@yandex.ru>
2020-08-23 21:59:57 +0300

    Исправил ошибки линтеринга

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
-  const { NODE_ENV, JWT_SECRET} = process.env;
+  const { NODE_ENV, JWT_SECRET } = process.env;
 
   if (!token) {
     return res.status(401).send({ message: 'Необходима авторизация' });
