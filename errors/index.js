c03a34dcef0915f09b84f91b7f6261de162ea751
Stas <bloodofabyss@yandex.ru>
2020-09-05 16:20:54 +0300

    Добавил конструкторы ошибок

---
+++ errors/index.js
@@ -0,0 +1,5 @@
+const BadRequestError = require('./bad-request-error');
+const NotFoundError = require('./not-found-error');
+const UnauthorizedError = require('./unauthorized-error');
+
+module.exports = { BadRequestError, NotFoundError, UnauthorizedError };
