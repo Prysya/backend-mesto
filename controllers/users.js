4dfc8036ab96b380970ad2cc31df5c300e5f603b
Stas <bloodofabyss@yandex.ru>
2020-09-05 16:29:55 +0300

    Переписал все контроллеры используя async/await

--- controllers/users.js
+++ controllers/users.js
@@ -1,144 +1,118 @@
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const escape = require('escape-html');
+const { createToken } = require('../utils/token');
 const User = require('../models/user');
 const schemaValidator = require('../utils/passwordValidator');
+const {
+  BadRequestError,
+  NotFoundError,
+} = require('../errors/index');
+const messages = require('../utils/messages');
 
-module.exports.getUsers = (req, res) => {
-  User.find({})
-    .then((users) => {
-      res.send({ data: users });
-    })
-    .catch(() => {
-      res.status(500).send({ message: 'На сервере произошла ошибка' });
-    });
-};
-
-module.exports.getUser = ({ params: { id } }, res) => {
-  User.findById(id)
-    .orFail(() => new Error('ID не найден'))
-    .then((user) => res.send({ data: user }))
-    .catch((err) => {
-      if (err.name === 'CastError' || err.message === 'ID не найден') {
-        return res.status(404).send({ message: 'Нет пользователя с таким id' });
-      }
-
-      return res.status(500).send({ message: 'На сервере произошла ошибка' });
-    });
-};
-
-module.exports.createUser = (req, res) => {
-  const {
-    name, about, avatar, email, password,
-  } = req.body;
-
-  if (!password || password.length < 8 || !schemaValidator.validate(password)) {
-    return res
-      .status(400)
-      .send({
-        message: 'Длинна пароля менее 8 символов, либо пароль не валиден',
-      });
+module.exports.getUsers = async (req, res, next) => {
+  try {
+    const users = await User.find({});
+    res.send({ data: users });
+  } catch (err) {
+    next(err);
   }
+};
 
-  return bcrypt
-    .hash(password, 10)
-    .then((hash) => User.create({
-      name,
-      about,
+module.exports.getUser = async ({ params: { id } }, res, next) => {
+  try {
+    const user = await User.findById(id).orFail(
+      () => new NotFoundError(messages.user.idIsNotFound),
+    );
+    res.send({ data: user });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.createUser = async (req, res, next) => {
+  try {
+    const {
+      name, about, avatar, email, password,
+    } = req.body;
+
+    if (
+      !password
+      || password.length < 8
+      || !schemaValidator.validate(password)
+    ) {
+      throw new BadRequestError(messages.user.passwordIsNotValid);
+    }
+
+    const hash = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      name: escape(name),
+      about: escape(about),
       avatar,
       email,
       password: hash,
-    }))
-    .then((user) => res.send({
+    });
+
+    res.send({
       data: {
         email: user.email,
         name: user.name,
         about: user.about,
         avatar: user.avatar,
       },
-    }))
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        return res
-          .status(400)
-          .send({ message: 'Ошибка валидации данных пользователя' });
-      }
-
-      if (err.name === 'MongoError' && err.code === 11000) {
-        return res
-          .status(409)
-          .send({ message: 'Данный email уже зарегестрирован' });
-      }
-
-      return res.status(500).send({ message: 'На сервере произошла ошибка' });
     });
+  } catch (err) {
+    next(new BadRequestError(err.message));
+  }
 };
 
-module.exports.updateUserData = (req, res) => {
-  const { name, about } = req.body;
+module.exports.updateUserData = async (req, res, next) => {
+  try {
+    const { name, about } = req.body;
 
-  User.findByIdAndUpdate(
-    req.user._id,
-    { name, about },
-    { new: true, runValidators: true },
-  )
-    .orFail(() => new Error('ID не найден'))
-    .then((user) => res.send({ data: user }))
-    .catch((err) => {
-      if (err.name === 'CastError' || err.message === 'ID не найден') {
-        return res.status(404).send({ message: 'Нет пользователя с таким id' });
-      }
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      { name: escape(name), about: escape(about) },
+      { new: true, runValidators: true },
+    ).orFail(() => new NotFoundError(messages.user.idIsNotFound));
 
-      if (err.name === 'ValidationError') {
-        return res
-          .status(400)
-          .send({ message: 'Ошибка валидации данных пользователя' });
-      }
-
-      return res.status(500).send({ message: 'На сервере произошла ошибка' });
-    });
+    res.send({ data: user });
+  } catch (err) {
+    next(new BadRequestError(err.message));
+  }
 };
 
-module.exports.updateUserAvatar = (req, res) => {
-  const { avatar } = req.body;
+module.exports.updateUserAvatar = async (req, res, next) => {
+  try {
+    const { avatar } = req.body;
 
-  User.findByIdAndUpdate(
-    req.user._id,
-    { avatar },
-    { new: true, runValidators: true },
-  )
-    .orFail(() => new Error('ID не найден'))
-    .then((user) => res.send({ data: user }))
-    .catch((err) => {
-      if (err.name === 'CastError' || err.message === 'ID не найден') {
-        return res.status(404).send({ message: 'Нет пользователя с таким id' });
-      }
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      { avatar },
+      { new: true, runValidators: true },
+    ).orFail(() => new NotFoundError(messages.user.idIsNotFound));
 
-      if (err.name === 'ValidationError') {
-        return res.status(400).send({ message: 'Ошибка валидации ссылки' });
-      }
-
-      return res.status(500).send({ message: 'На сервере произошла ошибка' });
-    });
+    res.send({ data: user });
+  } catch (err) {
+    next(new BadRequestError(err.message));
+  }
 };
 
-module.exports.login = (req, res) => {
-  const { email, password } = req.body;
-  const { NODE_ENV, JWT_SECRET } = process.env;
+module.exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
 
-  return User.findUserByCredentials(email, password)
-    .then((user) => {
-      const token = jwt.sign(
-        { _id: user._id },
-        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
-        { expiresIn: '7d' },
-      );
+    const user = await User.findUserByCredentials(email, password);
 
-      res
-        .cookie('jwt', token, {
-          maxAge: 3600000 * 24 * 7,
-          httpOnly: true,
-        })
-        .end();
-    })
-    .catch((err) => res.status(401).send({ message: err.message }));
+    const token = await createToken(user);
+
+    await res
+      .cookie('jwt', token, {
+        maxAge: 3600000 * 24 * 7,
+        httpOnly: true,
+      });
+
+    res.status(200).send({ message: messages.auth.authIsSuccess });
+  } catch (err) {
+    next(err);
+  }
 };
