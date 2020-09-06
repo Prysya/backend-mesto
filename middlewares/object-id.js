760c0d26fe9d1487c0c4216db8ccfd3462f6c8bd
Stas <bloodofabyss@yandex.ru>
2020-09-06 11:13:52 +0300

    Поменял :cardId на :id

--- middlewares/object-id.js
+++ middlewares/object-id.js
@@ -2,7 +2,7 @@ const { ObjectId } = require('mongodb');
 const messages = require('../utils/messages');
 const { BadRequestError } = require('../errors/index');
 
-module.exports.verifyCardId = (req, res, next) => (ObjectId.isValid(req.params.cardId)
+module.exports.verifyCardId = (req, res, next) => (ObjectId.isValid(req.params.id)
   ? next()
   : next(new BadRequestError(messages.card.idIsNotValid)));
 
