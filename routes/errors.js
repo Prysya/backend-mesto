bd7044e8c70553636830adaa8dfda43ba48364b5
Stas <bloodofabyss@yandex.ru>
2020-09-06 11:15:49 +0300

    Ошибка неизвестного маршрута теперь попадает в обработчик

--- routes/errors.js
+++ routes/errors.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
+const { NotFoundError } = require('../errors/index');
+const messages = require('../utils/messages');
 
-router.all('*', (req, res) => res.status(404).send({ message: 'Запрашиваемый ресурс не найден' }));
+router.all('*', (req, res, next) => {
+  next(new NotFoundError(messages.validation.notFound));
+});
 
 module.exports = router;
